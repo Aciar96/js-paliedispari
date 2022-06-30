fb6b9cfe1d6076408945ce676c8da8a568192a44
Aciar96 <alfredo.ciarmiello96@gmail.com>
2022-06-30 14:42:58 +0200

    fixed erorr

--- js/paridispari.js
+++ js/paridispari.js
@@ -13,28 +13,27 @@ Dichiariamo chi ha vinto.
 //5- calcolo se il risultato è pari o dispari 
 
 // 1 - chiedo all'utente di scegliere tra pari o dispari
-const quest = prompt("scegli pari o dispari")
+let quest = prompt("scegli pari o dispari").trim()
 console.log('scelta dell" utanete:' + quest)
-
+while(quest !== 'pari' && quest !== 'dispari'){
+    quest = prompt("scegli pari o dispari").trim()
+}
 // 2- creo una funzione che chiede all'utente un numero da 1 a 5 
 
-function getNumberFromUser(min = 1, max = 5) {
+
     let numUser = parseInt(prompt(`Inserisci un numero da ${min} a ${max}`))
 
     //verifico che il numero sia effettivamente un numero e che il minimo e il massimo siano quelli indicati dalla domanda (quindi min=1 max=5)
     while (isNaN(numUser) || numUser < min || numUser > max) {
         alert('Inserisci  un numero valido da 1 a 5')
     }
-    return numUser;
-}
-const numUser = getNumberFromUser()
 console.log('numero dell utante:'+ numUser)
 
 
 
 //3- creo un numero random per il pc da 1 a 5 
 function getRandomNumber(min = 1, max = 5) {
-    const RandomNumber = Math.floor(Math.random() * (max + min))
+    const RandomNumber = Math.floor(Math.random() * (max + 1 - min))
     return RandomNumber
 }
 const RandomNumber = getRandomNumber()
@@ -47,8 +46,7 @@ console.log('somma dei due numeri:' +sum)
 
 //5- calcolo se il risultato è pari o dispari 
  
-if(sum % 2 === 0){
-    console.log('risultato finale: pari')
-}else{
-    console.log('risultato finale:dispari')
+
+function isEven(sum){
+ return !(sum % 2);
 }
